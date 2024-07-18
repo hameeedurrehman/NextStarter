dba668b53e47168b28a29233dc35585c80297fa3
hameeedurrehman <hameedurrehman35@gmail.com>
2024-07-18 15:02:34 +0100

    ADD: config file

---
+++ components/config/CompanyDetails.tsx
@@ -0,0 +1,54 @@
+export const company = {
+  seotitle: "",
+  seodescription: "",
+  name: "",
+  address: {
+    building: "",
+    street: "",
+    town: "",
+    postcode: "",
+  },
+  cookies_name: "companyNameCookies",
+  apptitle: "",
+  apple_store_url: "#",
+  google_store_url: "#",
+  company_email: "info@companyname.co.uk",
+  tel_number: "01706 225300",
+  dpo_email: "info@companyname.co.uk",
+  dpo: "",
+  vat_number: "-",
+  compnay_number: "-",
+  website: "",
+  url: "",
+  dpo_reg_number: "-",
+  updated_on: "18/07/2024",
+  Copyright:
+    "© 2024 Company Name, Inc. All rights reserved.",
+  logo_url: "/assets/logo.png",
+  logo_alt: "Logo",
+  width: "",
+  height: "",
+  width_mobile: "",
+  height_mobile: "",
+  opening_hours: [
+    {
+      day: "Monday to Friday",
+      time: "5pm to 11pm",
+    },
+    {
+      day: "Saturday - Sunday",
+      time: "1pm to 11pm",
+    },
+  ],
+  contactus_task_url: "https://",
+  google_maps_url: "",
+  google_maps_iframe: "",
+
+  Instagram: "",
+  Facebook: "",
+  Twitter: "",
+};
+// Check List - make sure update below files before making this website live
+// 1- robots_txt: 'update robots.txt file domain details
+// 1-   robots_txt: 'update robots.txt file domain details
+// 2-  regenarate manifest.json file and icons and put in public folder
