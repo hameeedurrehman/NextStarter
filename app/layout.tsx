5ac3bab634b74b7b743ec1478cb1e1302c4c6c35
hameeedurrehman <hameedurrehman35@gmail.com>
2024-07-18 14:40:02 +0100

    ADD: toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Layout from "@/components/wrapper/Layout";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -42,7 +43,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Layout>{children}</Layout>
+        <Layout>
+          {children}
+          <Toaster />
+        </Layout>
       </body>
     </html>
   );
